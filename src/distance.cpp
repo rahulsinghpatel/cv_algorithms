5d3603a3364920545250804a0e649b9bc617e710
RAHUL PATEL <43598897+rahulsinghpatel@users.noreply.github.com>
2019-07-23 13:53:27 +0530

    Update distance.cpp

--- src/distance.cpp
+++ src/distance.cpp
@@ -4,6 +4,8 @@
 #include <limits.h>
 #include <string.h>
 #include <stdint.h>
+#include <cstdlib>
+using namespace std;
 //Forward declaration required due to CFFI's requirement to have unmangled symbols
 extern "C" {
 	int pairwise_diff(const double* a, const double* b, double* result, size_t awidth, size_t bwidth);
@@ -14,7 +16,7 @@ int pairwise_diff(const double* a, const double* b, double* result, size_t awidt
 	//Iterate over all (a,b) element pairs
 	for (size_t ax = 0; ax < awidth; ++ax) {
 		for (size_t bx = 0; bx < bwidth; ++bx) {
-			result[ax*awidth + bx] = std::abs((a[ax] - b[bx]));
+			result[ax*awidth + bx] = abs((a[ax] - b[bx]));
 		}	
 	}
 	return 0;
